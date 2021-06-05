66333e1a4f32d5dd6a2bba74aad20fb6a83768cc
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-05 22:48:43 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Team1 from "./components/teams/team1/team1.component.jsx";
+import Team2 from "./components/teams/team2/team2.component.jsx";
+import Team3 from "./components/teams/team3/team3.component.jsx";
+import "./App.scss";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="layout">
+        <input
+          name="nav"
+          type="radio"
+          className="nav home-radio"
+          id="home"
+          checked="checked"
+        />
+        <div className="page home-page">
+          <div className="page-contents">
+            <Team1 />
+          </div>
+        </div>
+        <label className="nav" for="home">
+          <span>1st Team</span>
+        </label>
+
+        <input name="nav" type="radio" className="about-radio" id="about" />
+        <div className="page about-page">
+          <div className="page-contents">
+            <Team2 />
+          </div>
+        </div>
+        <label className="nav" for="about">
+          <span>2rd Team</span>
+        </label>
+
+        <input name="nav" type="radio" className="contact-radio" id="contact" />
+        <div className="page contact-page">
+          <div className="page-contents">
+            <Team3 />
+          </div>
+        </div>
+        <label className="nav" for="contact">
+          <span>3rd Team</span>
+        </label>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/teams/team1/team1.component.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Data from "../../../data";
+import "../team.style.scss";
+
+export class Team1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerData: Data,
+    };
+  }
+
+  render() {
+    return (
+      <div className="table">
+        <table className="styled-table">
+          <thead >
+            <tr>
+              <th style={{paddingLeft:"100px"}}>Profile</th>
+              <th>Matchs</th>
+              <th>Runs</th>
+              <th>Wickets</th>
+            </tr>
+          </thead>
+          {this.state.playerData.team1.Players.map((player) => (
+            <tbody>
+              <tr className="stats">
+                <td id="profile">
+                  <img className="profile" src={player.img} alt="" />
+                 <h4> {player.name}</h4>
+                </td>
+                <td>{player.matches}</td>
+                <td>{player.runs}</td>
+                <td>{player.wickets}</td>
+              </tr>
+              <br />
+            </tbody>
+          ))}
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Team1;
